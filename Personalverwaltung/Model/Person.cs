f606195fefbf9a4f0bd008fb16dbedd3548a8d52
Kerim San <san.kerim@outlook.com>
2022-05-13 02:18:47 +0200

    Person Class erstellt und ViewModelBase verärbt

---
+++ Personalverwaltung/Model/Person.cs
@@ -0,0 +1,63 @@
+﻿using Personalverwaltung.ViewModel;
+
+namespace Personalverwaltung.Model
+{
+    public class Person : ViewModelBase
+    {
+        private string id;
+        public string Id
+        {
+            get => id;
+
+            set => SetProperty(ref id, value);
+        }
+
+        private string vorname;
+        public string Vorname
+        {
+            get => vorname;
+
+            set => SetProperty(ref vorname, value);
+        }
+
+        private string nachname;
+        public string Nachname
+        {
+            get => nachname;
+
+            set => SetProperty(ref nachname, value);
+        }
+
+        private string strasse;
+        public string Strasse
+        {
+            get => strasse; 
+
+            set => SetProperty(ref strasse, value);
+        }
+
+        private string ort;
+        public string Ort
+        {
+            get => ort;
+
+            set => SetProperty(ref ort, value);
+        }
+
+        private int plz;
+        public int Plz
+        {
+            get => plz;
+            
+            set => SetProperty(ref plz, value);
+        }
+
+        private string details;
+        public string Details
+        {
+            get => details;
+
+            set => SetProperty(ref details, value);
+        }
+    }
+}
