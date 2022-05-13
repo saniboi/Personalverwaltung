bf93e44152ae72f0cb34faabb62772cf86be464a
Kerim San <san.kerim@outlook.com>
2022-05-13 02:22:51 +0200

    MainViewModel wurde erstellt und LoadPersons methode um die liste zu befüllen von der xml datei

---
+++ Personalverwaltung/ViewModel/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows.Data;
+using System.Xml.Serialization;
+using Personalverwaltung.Model;
+
+namespace Personalverwaltung.ViewModel
+{
+    internal class MainViewModel
+    {
+        private ObservableCollection<Person> personList;
+        private ListCollectionView personView;
+
+        public ListCollectionView PersonView
+        {
+            get => personView;
+        }
+
+        private void LoadPersons(ref ObservableCollection<Person> liste)
+        {
+            if (File.Exists("C:\\Users\\sanke\\source\\repos\\Personalverwaltung\\Personalverwaltung\\Resources\\Persons.xml"))
+            {
+                FileStream fs = new FileStream("C:\\Users\\sanke\\source\\repos\\Personalverwaltung\\Personalverwaltung\\Resources\\Persons.xml", FileMode.Open);
+                XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<Person>));
+                liste = (ObservableCollection<Person>)serializer.Deserialize(fs);
+                fs.Close();
+            }
+        }
+
+        public MainViewModel()
+        {
+            personList = new ObservableCollection<Person>();
+            LoadPersons(ref personList);
+            personView = new ListCollectionView(personList);
+        }
+    }
+}
