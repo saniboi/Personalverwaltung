42a31d8bc8b640ddb7cbced1e0b96b6a89985836
Kerim San <san.kerim@outlook.com>
2022-05-19 17:43:54 +0200

    Pfad angepasst von XML File

--- Personalverwaltung/ViewModel/MainViewModel.cs
+++ Personalverwaltung/ViewModel/MainViewModel.cs
@@ -18,9 +18,9 @@ namespace Personalverwaltung.ViewModel
 
         private void LoadPersons(ref ObservableCollection<Person> liste)
         {
-            if (File.Exists("C:\\Users\\sanke\\source\\repos\\Personalverwaltung\\Personalverwaltung\\Resources\\Persons.xml"))
+            if (File.Exists("Persons.xml"))
             {
-                FileStream fs = new FileStream("C:\\Users\\sanke\\source\\repos\\Personalverwaltung\\Personalverwaltung\\Resources\\Persons.xml", FileMode.Open);
+                FileStream fs = new FileStream("Persons.xml", FileMode.Open);
                 XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<Person>));
                 liste = (ObservableCollection<Person>)serializer.Deserialize(fs);
                 fs.Close();
